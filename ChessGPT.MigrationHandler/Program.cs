e3d3df6ef7da966381646cd004ea0b9f5d03c581
Kaiden Brunke <700233885@fvtc.edu>
2024-02-23 19:51:46 -0600

    Created The Migration Handler

---
+++ ChessGPT.MigrationHandler/Program.cs
@@ -0,0 +1,16 @@
+﻿using ChessGPT.PL.Data;
+using Microsoft.AspNetCore.Builder;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+
+var builder = WebApplication.CreateBuilder(args);
+
+// Add services to the container.
+builder.Services.AddDbContextPool<ChessGPTEntities>(options =>
+options.UseSqlServer(builder.Configuration.GetConnectionString("DatabaseConnection"), builder => {
+    builder.EnableRetryOnFailure(5, TimeSpan.FromSeconds(10), null);
+}));
+
+var app = builder.Build();
+app.Run();
