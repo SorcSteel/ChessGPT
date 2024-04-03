7347d0b7f625911fa397eb2ed789e782573328f5
Kaiden Brunke <700233885@fvtc.edu>
2024-04-02 23:36:39 -0500

    Started Working on API

---
+++ ChessGPT.API/Controllers/UserController.cs
@@ -0,0 +1,80 @@
+﻿using ChessGPT.BL;
+using ChessGPT.BL.Models;
+using ChessGPT.PL.Data;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+
+namespace KB.DVDCentral.API.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class UserController : ControllerBase
+    {
+        private readonly ILogger<UserController> logger;
+        private readonly DbContextOptions<ChessGPTEntities> options;
+
+        public UserController(DbContextOptions<ChessGPTEntities> options)
+        {
+            this.options = options;
+        }
+
+        /// <summary>
+        /// Returns a List of Movies
+        /// </summary>
+        /// <returns></returns>
+
+        [HttpGet]
+        public IEnumerable<User> Get()
+        {
+            return new UserManager(options).Load();
+        }
+
+        [HttpGet("{id}")]
+        public User Get(Guid id)
+        {
+            return new UserManager(options).LoadById(id);
+        }
+
+        [HttpPost("{rollback?}")]
+        public int post([FromBody] User user, bool rollback = false)
+        {
+            try
+            {
+                return new UserManager(options).Insert(user, rollback);
+            }
+            catch (Exception)
+            {
+
+                throw;
+            }
+        }
+
+        [HttpPut("{id}/{rollback?}")]
+        public int put(Guid id, [FromBody] User user, bool rollback = false)
+        {
+            try
+            {
+                return new UserManager(options).Update(user, rollback);
+            }
+            catch (Exception)
+            {
+
+                throw;
+            }
+        }
+
+        [HttpDelete("{id}/{rollback?}")]
+        public int Delete(Guid id, bool rollback = false)
+        {
+            try
+            {
+                return new UserManager(options).Delete(id, rollback);
+            }
+            catch (Exception)
+            {
+
+                throw;
+            }
+        }
+    }
+}
---
+++ ChessGPT.API/Program.cs
@@ -0,0 +1,36 @@
+using ChessGPT.PL.Data;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.DependencyInjection;
+
+var builder = WebApplication.CreateBuilder(args);
+
+// Add services to the container.
+
+builder.Services.AddControllers();
+// Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
+builder.Services.AddEndpointsApiExplorer();
+builder.Services.AddSwaggerGen();
+
+//add conection information
+builder.Services.AddDbContextPool<ChessGPTEntities>(options =>
+{
+    options.UseSqlServer(builder.Configuration.GetConnectionString("DatabaseConnection"));
+    options.UseLazyLoadingProxies();
+});
+
+var app = builder.Build();
+
+// Configure the HTTP request pipeline.
+if (app.Environment.IsDevelopment())
+{
+    app.UseSwagger();
+    app.UseSwaggerUI();
+}
+
+app.UseHttpsRedirection();
+
+app.UseAuthorization();
+
+app.MapControllers();
+
+app.Run();
