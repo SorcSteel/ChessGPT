c85ee83717bf13ac08176366ee61bcf7f0f71e48
Logan Vang <500201348@fvtc.edu>
2024-04-11 10:14:23 -0500

    Added API Tests (unfinished)

---
+++ Chess.GPT.API.Test/utGame.cs
@@ -0,0 +1,53 @@
+﻿
+
+using ChessGPT.BL.Models;
+
+namespace Chess.GPT.API.Test
+{
+    [TestClass]
+    public class utGame : utBase
+    {
+        [TestMethod]
+        public async Task LoadTestAsync()
+        {
+            await base.LoadTestAsync<Game>(3);
+        }
+
+        [TestMethod]
+        public async Task InsertTestAsync()
+        {
+            Game game = new Game { GameName = "Test", GameTime = DateTime.Now, GameBoard = "rnbqkbnr/pppppppp/8/8/8/8/PPPPPPPP/RNBQKBNR", GameState = 'o' };
+            await base.InsertTestAsync<Game>(game);
+
+        }
+
+        [TestMethod]
+        public async Task InsertTestAsyncFail()
+        {
+            Game game = new Game { GameName = "First Game", GameTime = DateTime.Now, GameBoard = "rnbqkbnr/pppppppp/8/8/8/8/PPPPPPPP/RNBQKBNR", GameState = 'o' };
+            await base.InsertTestAsync<Game>(game);
+
+        }
+
+        [TestMethod]
+        public async Task DeleteTestAsync()
+        {
+            await base.DeleteTestAsync1<Game>(new KeyValuePair<string, string>("GameName", "First Game"));
+        }
+
+        [TestMethod]
+        public async Task LoadByIdTestAsync()
+        {
+            await base.LoadByIdTestAsync<Game>(new KeyValuePair<string, string>("GameName", "First Game"));
+        }
+
+        [TestMethod]
+        public async Task UpdateTestAsync()
+        {
+            Game game = new Game { GameName = "Test", GameTime = DateTime.Now, GameBoard = "rnbqkbnr/pppppppp/8/8/8/8/PPPPPPPP/RNBQKBNR", GameState = 'w' };
+            await base.UpdateTestAsync<Game>(new KeyValuePair<string, string>("GameState", "o"), game);
+
+        }
+
+    }
+}
---
+++ Chess.GPT.API.Test/utUserGame.cs
@@ -0,0 +1,53 @@
+﻿
+
+using ChessGPT.BL.Models;
+
+namespace Chess.GPT.API.Test
+{
+    [TestClass]
+    public class utUserGame : utBase
+    {
+        [TestMethod]
+        public async Task LoadTestAsync()
+        {
+            await base.LoadTestAsync<UserGame>(6);
+        }
+
+        [TestMethod]
+        public async Task InsertTestAsync()
+        {
+            UserGame userGame = new UserGame { UserId = Guid.NewGuid(), GameId = Guid.NewGuid(), Color = 'B' };
+            await base.InsertTestAsync<UserGame>(userGame);
+
+        }
+
+        [TestMethod]
+        public async Task InsertTestAsyncFail()
+        {
+            UserGame userGame = new UserGame { UserId = Guid.Parse("4882ba82-2809-4f40-848d-5230a1d398de"), GameId = Guid.NewGuid(), Color = 'B'  };
+            await base.InsertTestAsync<UserGame>(userGame);
+
+        }
+
+        [TestMethod]
+        public async Task DeleteTestAsync()
+        {
+            await base.DeleteTestAsync1<UserGame>(new KeyValuePair<string, string>("Description", "Other"));
+        }
+
+        [TestMethod]
+        public async Task LoadByIdTestAsync()
+        {
+            await base.LoadByIdTestAsync<UserGame>(new KeyValuePair<string, string>("Description", "Other"));
+        }
+
+        [TestMethod]
+        public async Task UpdateTestAsync()
+        {
+            UserGame userGame = new UserGame { Color = 'B' };
+            await base.UpdateTestAsync<UserGame>(new KeyValuePair<string, string>("Description", "Other"), userGame);
+
+        }
+
+    }
+}
