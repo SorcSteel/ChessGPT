d8c404d83878e9eeddafa5b3db4ca459ee5ed4c7
Kaiden Brunke <700233885@fvtc.edu>
2024-04-15 23:48:10 -0500

    Added Console App To test SignalR Hub

---
+++ ChessGPT.ConsoleApp/Program.cs
@@ -0,0 +1,42 @@
+﻿using ChessGPT.ConsoleApp;
+
+internal class Program
+{
+
+    private static string DrawMenu()
+    {
+        Console.WriteLine("Which operation do you wish to perform");
+        Console.WriteLine("connect to a channel (c)");
+        Console.WriteLine("send a message to the channel (s)");
+        Console.WriteLine("exit (x)");
+
+        string operation = Console.ReadLine();
+        return operation;
+    }
+    private static void Main(string[] args)
+    {
+        string user = "Kaiden";
+        string hubAddress = "https://localhost:7230/chessgpthub";
+
+        string operation = DrawMenu();
+
+        var signalRConnection = new SignalRConnection(hubAddress);
+
+        while (operation != "x")
+        {
+            switch (operation)
+            {
+                case "x":
+                    break;
+
+                case "c":
+                    signalRConnection.ConnectToChannel(user);
+                    break;
+
+                case "d":
+                    break;
+            }
+            operation = DrawMenu();
+        }
+    }
+}
---
+++ ChessGPT.ConsoleApp/SignalRConnection.cs
@@ -0,0 +1,50 @@
+﻿using Microsoft.AspNetCore.SignalR.Client;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ChessGPT.ConsoleApp
+{
+    internal class SignalRConnection
+    {
+        private string hubAdress;
+        HubConnection _connection;
+        string user;
+
+        public SignalRConnection(string hubAdress)
+        {
+            this.hubAdress = hubAdress;
+        }
+        public void Start()
+        {
+            _connection = new HubConnectionBuilder()
+                .WithUrl(hubAdress)
+                .Build();
+
+            _connection.On<string, string>("ReceiveMessage", (s1, s2) => OnSend(s1, s2));
+
+            _connection.StartAsync();
+        }
+
+        private void OnSend(string user, string message)
+        {
+            Console.WriteLine(user = ": " + message);
+        }
+
+        public void ConnectToChannel(string user)
+        {
+            Start();
+            string message = user + " Connected";
+            try
+            {
+                _connection.InvokeAsync("SendMessage", "System", message);
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine($"Error: {ex.Message}");
+            }
+        }
+    }
+}
